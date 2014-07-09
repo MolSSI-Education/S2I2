183dae0bd48af4f3c7ed16c3a4e927cd74cfb56c
Eduard Valeyev <eduard@valeyev.net>
2014-07-09 12:52:35 -0400

    Can take geometry filename from the command line; can parse standard XYZ files

--- hf.v3/scf.cc
+++ hf.v3/scf.cc
@@ -58,8 +58,9 @@ int main(int argc, char *argv[]) {
     /*** initialize molecule         ***/
     /*** =========================== ***/
 
-    // read geometry from a file
-    std::vector<Atom> atoms = read_geometry("h2o.geom");
+    // read geometry from a file; by default read from h2o.geom, else take filename (.xyz or .geom) from the command line
+    const auto filename = (argc > 1) ? argv[1] : "h2o.geom";
+    std::vector<Atom> atoms = read_geometry(filename);
 
     // count the number of electrons
     auto nelectron = 0;
@@ -237,20 +238,75 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+// this reads the geometry in the same format used in older versions of the code
+std::vector<Atom> read_dotgeom(std::istream& is) {
+  size_t natom;
+  is >> natom;
+
+  std::vector<Atom> atoms(natom);
+  for (auto i = 0; i < natom; i++)
+    is >> atoms[i].atomic_number >> atoms[i].x >> atoms[i].y >> atoms[i].z;
+
+  return atoms;
+}
+
+// this reads the geometry in the standard xyz format supported by most chemistry software
+std::vector<Atom> read_dotxyz(std::istream& is) {
+  size_t natom;
+  is >> natom;
+
+  std::string comment;
+  std::getline(is, comment);
+
+  std::vector<Atom> atoms(natom);
+  for (auto i = 0; i < natom; i++) {
+    std::string element_label;
+    double x, y, z;
+    is >> element_label >> x >> y >> z;
+
+    // .xyz files report element labels, hence convert to atomic numbers
+    int Z;
+    if (element_label == "H")
+      Z = 1;
+    else if (element_label == "C")
+      Z = 6;
+    else if (element_label == "N")
+      Z = 7;
+    else if (element_label == "O")
+      Z = 8;
+    else if (element_label == "F")
+      Z = 9;
+    else if (element_label == "S")
+      Z = 16;
+    else if (element_label == "Cl")
+      Z = 17;
+    else {
+      std::cerr << "read_dotxyz: element label \"" << element_label << "\" is not recognized" << std::endl;
+      throw "Did not recognize element label in .xyz file";
+    }
+
+    atoms[i].atomic_number = Z;
+
+    // .xyz files report Cartesian coordinates in angstroms; convert to bohr
+    const auto angstrom_to_bohr = 1 / 0.52917721092; // 2010 CODATA value
+    atoms[i].x = x * angstrom_to_bohr;
+    atoms[i].y = y * angstrom_to_bohr;
+    atoms[i].z = z * angstrom_to_bohr;
+  }
+
+  return atoms;
+}
 
 std::vector<Atom> read_geometry(const std::string& filename) {
 
   std::ifstream is(filename);
   assert(is.good());
 
-  size_t natom;
-  is >> natom;
-
-  std::vector<Atom> atoms(natom);
-  for (int i = 0; i < natom; i++)
-    is >> atoms[i].atomic_number >> atoms[i].x >> atoms[i].y >> atoms[i].z;
-
-  return atoms;
+  // check the extension: if .xyz, assume the xyz format, otherwise use the same format used by hf.v1
+  if ( filename.rfind(".xyz") != std::string::npos)
+    return read_dotxyz(is);
+  else
+    return read_dotgeom(is);
 }
 
 std::vector<libint2::Shell> make_sto3g_basis(const std::vector<Atom>& atoms) {
