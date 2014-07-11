e5e0ff56ec94e3888f7b4a783127c34e50718574
T. Daniel Crawford <crawdad@vt.edu>
2014-07-11 09:01:43 -0400

    Adding demos for S2I2 school.

---
+++ eigenvalue/print_mat.cc
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+void print_mat(double **a, int m, int n,FILE *out)
+{
+      int ii,jj,kk,nn,ll;
+      int i,j,k;
+
+      ii=0;jj=0;
+L200:
+      ii++;
+      jj++;
+      kk=10*jj;
+      nn=n;
+      if (nn > kk) nn=kk;
+      ll = 2*(nn-ii+1)+1;
+      fprintf (out,"\n");
+      for (i=ii; i <= nn; i++) fprintf(out,"       %5d",i);
+      fprintf (out,"\n");
+      for (i=0; i < m; i++) {
+         fprintf (out,"\n%5d",i+1);
+         for (j=ii-1; j < nn; j++) {
+            fprintf (out,"%12.7f",a[i][j]);
+            }
+         }
+      fprintf (out,"\n");
+      if (n <= kk) {
+         fflush(out);
+         return;
+         }
+      ii=kk; goto L200;
+}
