f0bfd708d2c19a8b375af19ee2211fb432637242
Robert J. Harrison <rjharrison@gmail.com>
2014-07-19 15:32:29 -0400

    Example summation using multiple accumualtors and orders

---
+++ CUDA/basic/sum.cu
@@ -0,0 +1,257 @@
+#include <math.h>
+#include <cstdio>
+#include <typeinfo>
+#include <cstring>
+#include <cuda.h>
+
+using namespace std;
+
+#define CHECK(test) \
+  if (test != cudaSuccess) throw "error";
+
+template <typename AccumulatorType, typename DataType>
+__global__ void SumWithinBlocks(int n, const DataType* data, AccumulatorType* blocksums) {
+  int nthread = blockDim.x*gridDim.x;
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  __shared__ AccumulatorType sdata[1024];  // 1024=max#blocks
+
+  // First every thread in every block computes partial sum over rest of vector
+  AccumulatorType st=0.0;
+  while (i < n) {
+    st += data[i];
+    i+=nthread;
+  }
+  sdata[threadIdx.x] = st;
+  __syncthreads();
+
+  // Now do binary tree sum within a block
+  int tid = threadIdx.x;
+  for (unsigned int s=blockDim.x>>1; s>0; s>>=1) {
+    if (tid < s) {
+      sdata[tid] += sdata[tid + s];
+    }
+    __syncthreads();
+  }
+  if (tid==0) blocksums[blockIdx.x] = sdata[0];
+}
+
+// Computes sum of n device-resident data elements returning value to host  
+template <typename AccumulatorType, typename DataType>
+AccumulatorType Sum(int n, const DataType* d_data) {
+  const int nblocks = 128;
+  const int nthreads=1024;
+
+  AccumulatorType *d_sums;
+  CHECK(cudaMalloc(&d_sums, sizeof(AccumulatorType)));
+
+  SumWithinBlocks<<<nblocks,nthreads>>>(n, d_data, d_sums);
+  SumWithinBlocks<<<1,nblocks>>>(nblocks, d_sums, d_sums);
+
+  AccumulatorType sum;
+  CHECK(cudaMemcpy(&sum, d_sums, sizeof(AccumulatorType), cudaMemcpyDeviceToHost));
+  cudaFree(d_sums);
+  return sum;
+}
+
+__device__ __host__ double f(int i) {
+  double t = 1.0/(i+1);
+  if (i&0x1) t=-t;
+  return t;
+}
+
+template <typename T>
+class KahanAccumulator {
+  T sum;
+  T c;
+public:
+  __device__ __host__
+  KahanAccumulator() {} // Must be empty for use in shared memory
+
+  __device__ __host__
+  KahanAccumulator(T s) : sum(s), c(0) {}
+
+  __device__ __host__
+  KahanAccumulator& operator=(const T s) {
+    sum = s;
+    c = 0;
+    return *this;
+  }
+
+  __device__ __host__
+  KahanAccumulator& operator+=(const T input) {
+    T y = input - c;
+    T t = sum + y;
+    c = (t - sum) - y;
+    sum = t;
+    return *this;
+  }
+
+  __device__ __host__
+  KahanAccumulator& operator+=(const KahanAccumulator& input) {
+    (*this) += input.sum;
+    (*this) += -input.c;
+    return *this;
+  }
+
+  __device__ __host__
+  operator T() const {
+    return sum;
+  }
+};
+
+
+// Minimal double-double accumuator snarfed from Bailey's full double-double implementation
+class DD {
+  double x[2];
+
+  __device__ __host__
+  inline double quick_two_sum(double a, double b, double &err) {
+    double s = a + b;
+    err = b - (s - a);
+    return s;
+  }
+    
+  __device__ __host__
+  inline double two_sum(double a, double b, double &err) {
+    double s = a + b;
+    double bb = s - a;
+    err = (a - (s - bb)) + (b - bb);
+    return s;
+  }
+        
+public:
+
+  __device__ __host__
+  DD() {} // Default constructor must be empty for use in Nvidia shared memory
+
+  __device__ __host__
+  DD(double v) {x[0]=v; x[1]=0;}
+
+  __device__ __host__
+  DD& operator=(const double a) {
+    x[0] = a;
+    x[1] = 0;
+    return *this;
+  }
+        
+  __device__ __host__
+  DD& operator=(const DD& a) {
+    x[0] = a.x[0];
+    x[1] = a.x[1];
+    return *this;
+  }
+        
+  __device__ __host__
+  DD& operator+=(const DD &a) {
+    double s, e;
+    s = two_sum(x[0], a.x[0], e);
+    e += x[1];
+    e += a.x[1];
+    x[0] = quick_two_sum(s, e, x[1]);
+    return *this;
+  }        
+
+  __device__ __host__
+  DD& operator+=(const double a) {
+    double s, e;
+    s = two_sum(x[0], a, e);
+    e += x[1];
+    x[0] = quick_two_sum(s, e, x[1]);
+    return *this;
+  }        
+
+  __device__ __host__
+  operator double() const {
+    return x[0];
+  }
+};
+
+  
+// Initialize the on-device data to f(i), i=0..n-1
+template <typename DataType>
+__global__ void Initialize(int n, DataType* d) {
+  int nthread = blockDim.x*gridDim.x;
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  while (i < n) {
+    d[i] = DataType(f(i));
+    i+=nthread;
+  }
+}
+
+
+template <typename AccumulatorType, typename DataType>
+void test(const int n, const double exact) {
+  DataType *d_data;
+  CHECK(cudaMalloc(&d_data, n*sizeof(DataType)));
+
+  Initialize<<<128,1024>>>(n, d_data);
+
+  AccumulatorType sum = Sum<AccumulatorType,DataType>(n, d_data);
+
+  const char* aname = typeid(AccumulatorType).name();
+  if (strcmp(aname,"16KahanAccumulatorIfE") == 0)  aname = "KahanF";
+  if (strcmp(aname,"16KahanAccumulatorIdE") == 0)  aname = "KahanD";
+  if (strcmp(aname,"2DD") == 0)  aname = "DD";
+
+  printf("%6s   %6s     %.16e  %.1e\n", 
+	 typeid(DataType).name(), 
+	 aname,
+	 DataType(sum),
+	 fabs(DataType(sum)-exact));
+}
+
+int main() {
+  // From Maple
+  // sum((-1)^k/(k+1), k = 0..N-1)=ln(2)-(1/2)*(-1)^N*(Psi((1/2)*N+1)-Psi((1/2)*N+1/2))
+  // limit as N->infinity is ln(2)
+  const int n = 1024*1024*100;
+  const double exact = 0.69314717579157375012334966576;
+
+  double dsum=0.0;
+  KahanAccumulator<double> kdsum(0.0);
+  KahanAccumulator<float> kfsum(0.0);
+  DD ddsum(0.0);
+  for (int i=0; i<n; i++) {
+    double t = f(i);
+    dsum += t;
+    kdsum+=t;
+    kfsum+=float(t);
+    ddsum += t;
+  }
+
+  double rdsum=0.0;
+  KahanAccumulator<double> rkdsum(0.0);
+  KahanAccumulator<float> rkfsum(0.0);
+  DD rddsum(0.0);
+  for (int i=n-1; i>=0; i--) {
+    double t = f(i);
+    rdsum += t;
+    rkdsum+=t;
+    rkfsum+=float(t);
+    rddsum += t;
+  }
+  
+  printf(" DatType  AccType          Result            Error\n");
+  printf(" -------  -------   ---------------------   -------\n");
+  test<float,float>(n,exact);
+  test<float,double>(n,exact);
+  test<double,float>(n,exact);
+  test<double,double>(n,exact);
+  test<KahanAccumulator<float>,float>(n,exact);
+  test<KahanAccumulator<double>,double>(n,exact);
+  test<DD,double>(n,exact);
+
+  printf("Host double         %.16e  %.1e\n", dsum, fabs(dsum-exact));
+  printf("Host Kahan<float>   %.16e  %.1e\n", double(kfsum), fabs(double(kfsum)-exact));
+  printf("Host Kahan<double>  %.16e  %.1e\n", double(kdsum), fabs(double(kdsum)-exact));
+  printf("Host DD st          %.16e  %.1e\n", double(ddsum), fabs(double(ddsum)-exact));
+  printf("reversed order\n");
+  printf("Host double         %.16e  %.1e\n", rdsum, fabs(rdsum-exact));
+  printf("Host Kahan<float>   %.16e  %.1e\n", double(rkfsum), fabs(double(rkfsum)-exact));
+  printf("Host Kahan<double>  %.16e  %.1e\n", double(rkdsum), fabs(double(rkdsum)-exact));
+  printf("Host DD st          %.16e  %.1e\n", double(rddsum), fabs(double(rddsum)-exact));
+
+  return 0;
+}
+
+    
