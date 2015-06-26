3603c6ff3dc29fbeea59c2c1e3f69891c6dd7caa
Eduard Valeyev <eduard@valeyev.net>
2015-06-26 09:51:26 -0400

    Oops

--- core/run_axpy.py
+++ core/run_axpy.py
@@ -2,7 +2,7 @@
 
 import os
 
-for kernel in ['plain', 'blas', 'eigen']:
+for kernel in ['plain', 'blas']:
     for k in range(4,25):
         cmd = './axpy ' + str(2**k) + ' ' + str(2**(30-k)) + ' ' + kernel
         os.system(cmd)
